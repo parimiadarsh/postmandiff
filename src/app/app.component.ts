9d8c48449391f5b9518c567ee3e5fa42b29edbfe
parimiadarsh <parimiadarsh@gmail.com>
2025-12-19 00:39:58 -0500

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from "./homepage/homepage.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomepageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'postmandiff';
+}
